010a932fc127455a1ede2f0f0f1252bd457f9f84
Michael <wil13040@byui.edu>
2019-06-22 22:46:45 -0600

    Organized Code and fixed IF statements

--- wk9p/index.js
+++ wk9p/index.js
@@ -21,114 +21,84 @@ express()
     if (mail == 'stampLetter' && weight <= 3.5) {
       if (weight <= 1) {
         result = 0.55;
-      }
-      else if (weight <= 2) {
+      } else if (weight <= 2) {
         result = 0.70.toFixed(2);
-      }
-      else if (weight <= 3) {
+      } else if (weight <= 3) {
         result = 0.85;
-      }
-      else if (weight <= 3.5) {
+      } else if (weight <= 3.5) {
         result = 1.00.toFixed(2);
       }
-    }
-    else if (mail == 'meterLetter' && weight <= 3.5) {
+    } else if (mail == 'meterLetter' && weight <= 3.5) {
       if (weight <= 1) {
-        result = 0.55;
+        result = 0.50.toFixed(2);
+      } else if (weight <= 2) {
+        result = 0.65;
+      } else if (weight <= 3) {
+        result = 0.80.toFixed(2);;
+      } else if (weight <= 3.5) {
+        result = 0.95
       }
-      else if (weight <= 2) {
-        result = 0.70.toFixed(2);
-      }
-      else if (weight <= 3) {
-        result = 0.85;
-      }
-      else if (weight <= 3.5) {
-        result = 1.00.toFixed(2);
-      }
-    }
-    else if ((mail == 'largeEnv') || (mail == 'stampedLetter' && weight > 3.51) || (mail == 'meterLetter' && weight > 3.5)) {
+    } else if ((mail == 'largeEnv') || (mail == 'stampLetter' && weight > 3.5) || (mail == 'meterLetter' && weight > 3.5)) {
       if (weight <= 1) {
         result = 1.00.toFixed(2);
-      }
-      else if (weight <= 2) {
+      } else if (weight <= 2) {
         result = 1.15;
-      }
-      else if (weight <= 3) {
+      } else if (weight <= 3) {
         result = 1.30.toFixed(2);
-      }
-      else if (weight <= 4) {
+      } else if (weight <= 4) {
         result = 1.45;
-      }
-      else if (weight <= 5) {
+      } else if (weight <= 5) {
         result = 1.60.toFixed(2);
-      }
-      else if (weight <= 6) {
+      } else if (weight <= 6) {
         result = 1.75;
-      }
-      else if (weight <= 7) {
+      } else if (weight <= 7) {
         result = 1.90.toFixed(2);
-      }
-      else if (weight <= 8) {
+      } else if (weight <= 8) {
         result = 2.05;
-      }
-      else if (weight <= 9) {
+      } else if (weight <= 9) {
         result = 2.20.toFixed(2);
-      }
-      else if (weight <= 10) {
+      } else if (weight <= 10) {
         result = 2.35;
-      }
-      else  if (weight <= 11) {
+      } else if (weight <= 11) {
         result = 2.50.toFixed(2);
-      }
-      else if (weight <= 12) {
+      } else if (weight <= 12) {
         result = 2.65;
-      }
-      else if (weight <= 13) {
+      } else if (weight <= 13) {
         result = 2.80.toFixed(2);
+      } else {
+        result = 'Package is too large!'
       }
-    }
-    else if (mail == 'package') {
+    } else if (mail == 'package') {
       if (weight <= 1) {
         result = 3.66;
-      }
-      else if (weight <= 2) {
+      } else if (weight <= 2) {
         result = 3.66;
-      }
-      else if (weight <= 3) {
+      } else if (weight <= 3) {
         result = 3.66;
-      }
-      else if (weight <= 4) {
+      } else if (weight <= 4) {
         result = 3.66;
-      }
-      else if (weight <= 5) {
+      } else if (weight <= 5) {
         result = 4.39;
-      }
-      else if (weight <= 6) {
+      } else if (weight <= 6) {
         result = 4.39;
-      }
-      else if (weight <= 7) {
+      } else if (weight <= 7) {
         result = 4.39;
-      }
-      else if (weight <= 8) {
+      } else if (weight <= 8) {
         result = 4.39;
-      }
-      else if (weight <= 9) {
+      } else if (weight <= 9) {
         result = 4.39;
-      }
-      else if (weight <= 10) {
+      } else if (weight <= 10) {
         result = 5.19;
-      }
-      else if (weight <= 11) {
+      } else if (weight <= 11) {
         result = 5.19;
-      }
-      else if (weight <= 12) {
+      } else if (weight <= 12) {
         result = 5.19;
-      }
-      else if (weight <= 13) {
+      } else if (weight <= 13) {
         result = 5.71;
+      } else {
+        result = 'Package is too large!'
       }
-    }
-    else {
+    } else {
       result = 'Error';
     }
 
@@ -137,12 +107,14 @@ express()
     console.log("The type of packge selected is: " + mail)
     console.log("The overall cost is: " + result)
 
-    var total = {weight: weight, mail: mail, result: result};
+    var total = {
+      weight: weight,
+      mail: mail,
+      result: result
+    };
 
     res.render("pages/results", total);
     res.end();
-
-    //res.setHeader('Content-Type', 'application/json');
 
     res.end(JSON.stringify({
       result: result
